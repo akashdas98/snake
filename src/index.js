7f6ad7a53891b458000bd635c6cee9553513f862
akashdas98 <akash42662012@gmail.com>
2019-11-21 13:17:35 +0530

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.height = 500;
+canvas.width = 500;
+c.fillStyle = "#13ba2c";
+c.fillRect(0, 0, 15, 15);
